e48748f8b514c0cf0944d3f23d4dfa5554aa46c8
Mateus Marques <marqs.mts@gmail.com>
2026-04-06 15:30:08 -0300

    Created the lval struct and functions

--- parsing.c
+++ parsing.c
@@ -7,6 +7,53 @@
 
 #include "mpc.h"
 
+/* enumeration of possible lval types */
+enum { LVAL_NUM, LVAL_ERR };
+
+/* enumeration of possible error types */
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+
+typedef struct {
+    int type;
+    int64_t num;
+    int err;
+} lval;
+
+lval lval_num(int64_t x) {
+    lval v;
+    v.type = LVAL_NUM;
+    v.num = x;
+    return v;
+}
+
+lval lval_err(int x) {
+    lval v;
+    v.type = LVAL_ERR;
+    v.err = x;
+    return v;
+}
+
+void lval_print(lval v) {
+    switch (v.type) {
+    case LVAL_NUM:
+        printf("%li", v.num);
+        break;
+    case LVAL_ERR:
+        switch(v.err) {
+        case LERR_DIV_ZERO:
+            printf("Error: Division by zero!\n");
+            break;
+        case LERR_BAD_OP:
+            printf("Error: Invalid operator!\n");
+            break;
+        case LERR_BAD_NUM:
+            printf("Error: Invalid number!\n");
+            break;
+        }
+        break;
+    }
+}
+
 int64_t eval_op(int64_t x, char *op, int64_t y) {
     if (strcmp(op, "+") == 0) { return x + y; }
     if (strcmp(op, "-") == 0) { return x - y; }
