b02e29d2164bb169dbbf5a3e5f67502238acd1e5
Mateus Marques <marqs.mts@gmail.com>
2026-05-12 16:50:02 -0300

    TCO for the apply case. But we have more leaks and UAF in valgrind (not caused by let*)

--- impls/lispc/step5_tco.c
+++ impls/lispc/step5_tco.c
@@ -8,126 +8,12 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-mal_obj_t *mal_eval(mal_env_t *env, mal_obj_t *node);
-
 char *mal_read() {
     char *input = readline("user> ");
     if (input) {
         add_history(input);
     }
     return input;
-}
-
-void mal_print(mal_obj_t *root) {
-    if (root && root->type != MAL_EMPTY) {
-        mal_obj_print(root);
-        putchar('\n');
-    }
-}
-
-mal_obj_t *mal_eval_noop(mal_obj_t *node) {
-    mal_obj_retain(node);
-    return node;
-}
-
-// assumes that node->type == MAL_SYMBOL
-mal_obj_t *mal_eval_symbol(mal_env_t *env, mal_obj_t *node) {
-    mal_obj_t *value = NULL;
-    MAL_OBJ_ASSERT(mal_env_get(env, node->data.symbol, &value),
-                   "Error: Symbol '%s' not found", mal_obj_sprint(node));
-    mal_obj_retain(value);
-    return value;
-}
-
-mal_obj_t *mal_handle_apply(mal_env_t *env, mal_obj_t *list_obj) {
-    mal_list_t *list = list_obj->data.list;
-    int list_len = mal_list_len(list);
-    mal_obj_t *zeroth = *mal_list_get(list, 0);
-
-    mal_obj_t *func = mal_eval(env, zeroth);
-    if (func->type == MAL_ERROR) return func;
-    if (!(func->type == MAL_BUILTIN || func->type == MAL_FUNCTION))
-    {
-        mal_obj_t *err = mal_obj_error_format(
-            "Error: Parameter '%s' cannot be used as a function to evaluate",
-            mal_obj_sprint(func));
-        mal_obj_release(func);
-        return err;
-    }
-
-    mal_list_t *eval_args = mal_list_create(16);
-    for (int i = 1; i < list_len; i++) {
-        mal_obj_t **arg_ptr = mal_list_get(list, i);
-        mal_obj_t *arg = mal_eval(env, *arg_ptr);
-        if (arg->type == MAL_ERROR) {
-            for (int j = 0; j < mal_list_len(eval_args); j++) {
-                mal_obj_release(*(mal_list_get(eval_args, j)));
-            }
-            mal_list_free(eval_args);
-            mal_obj_release(func);
-            return arg;
-        }
-        mal_list_push_back(eval_args, arg);
-    }
-
-    mal_obj_t *final_result = NULL;
-
-    if (func->type == MAL_BUILTIN) {
-        final_result = func->data.builtin_fn(eval_args);
-    }
-    else if (func->type == MAL_FUNCTION) {
-        mal_closure_t *f = func->data.function;
-        mal_obj_t *err_feedback = NULL;
-        mal_env_t *call_env = mal_env_create(f->env);
-
-        if (mal_env_bind(call_env, f->params->data.list, eval_args, &err_feedback)) {
-            final_result = mal_eval(call_env, f->body);
-        } else {
-            final_result = err_feedback;
-        }
-        mal_env_release(call_env);
-    }
-
-    for (int i = 0; i < mal_list_len(eval_args); i++) {
-        mal_obj_release(*(mal_list_get(eval_args, i)));
-    }
-    mal_list_free(eval_args);
-    mal_obj_release(func);
-
-    return final_result;
-}
-
-// assumes 0th element of MAL_LIST is the symbol "def!"
-// we expect (def! symbol_name (expr arg1 arg2))
-mal_obj_t *mal_handle_def(mal_env_t *env, mal_obj_t *list_obj) {
-    mal_list_t *list = list_obj->data.list;
-    MAL_OBJ_ASSERT(mal_list_len(list) == 3,
-                   "Error: def! expects 2 arguments. Got %d", mal_list_len(list)-1);
-
-    mal_obj_t *first = *mal_list_get(list, 1);
-    MAL_OBJ_ASSERT(first->type == MAL_SYMBOL,
-                   "First element '%s' of def! is not a symbol", mal_obj_sprint(first));
-
-    mal_obj_t *second = *mal_list_get(list, 2);
-    mal_obj_t *result = mal_eval(env, second);
-
-    if (result->type != MAL_ERROR) {
-        string_t key = first->data.symbol;
-        mal_env_set(env, key, result);
-    }
-    return result;
-}
-
-
-mal_obj_t *mal_handle_fn(mal_env_t *env, mal_obj_t *list_obj) {
-    mal_list_t *list = list_obj->data.list;
-    MAL_OBJ_ASSERT(mal_list_len(list) == 3,
-                   "Error: fn* expects 2 arguments. Got %d", mal_list_len(list)-1);
-
-    mal_obj_t *binds = *mal_list_get(list, 1);
-    mal_obj_t *body = *mal_list_get(list, 2);
-
-    return mal_obj_function(binds, body, env);
 }
 
 #define EVAL_RETURN(expr) do { mal_obj_t *_r = (expr); mal_env_release(env); return _r; } while(0)
@@ -142,11 +28,18 @@ mal_obj_t *mal_eval(mal_env_t *env, mal_obj_t *node) {
         if (!node) EVAL_RETURN(mal_obj_empty());
         switch (node->type) {
         case MAL_SYMBOL:
-            EVAL_RETURN(mal_eval_symbol(env, node));
+        {
+            mal_obj_t *value = NULL;
+            MAL_EVAL_ASSERT(mal_env_get(env, node->data.symbol, &value),
+                           "Error: Symbol '%s' not found", mal_obj_sprint(node));
+            mal_obj_retain(value);
+            EVAL_RETURN(value);
+        }
         case MAL_LIST:
         {
             mal_list_t *list = node->data.list;
-            if (mal_list_is_empty(list)) {
+            int listlen = mal_list_len(list);
+            if (listlen == 0) {
                 EVAL_RETURN(mal_obj_list(0));
             }
 
@@ -179,16 +72,34 @@ mal_obj_t *mal_eval(mal_env_t *env, mal_obj_t *node) {
                 }
                 if (symblen == 4 && strncmp(symbol, "def!", 4) == 0)
                 {
-                    EVAL_RETURN(mal_handle_def(env, node));
+
+                    MAL_OBJ_ASSERT(listlen == 3,
+                                   "Error: def! expects 2 arguments. Got %d", mal_list_len(list)-1);
+
+                    mal_obj_t *first = *mal_list_get(list, 1);
+                    MAL_OBJ_ASSERT(first->type == MAL_SYMBOL,
+                                   "First element '%s' of def! is not a symbol", mal_obj_sprint(first));
+
+                    mal_obj_t *second = *mal_list_get(list, 2);
+                    mal_obj_t *result = mal_eval(env, second);
+
+                    if (result->type != MAL_ERROR) {
+                        string_t key = first->data.symbol;
+                        mal_env_set(env, key, result);
+                    }
+                    EVAL_RETURN(result);
                 }
                 if (symblen == 3 && strncmp(symbol, "fn*", 3) == 0)
                 {
-                    EVAL_RETURN(mal_handle_fn(env, node));
+                    MAL_OBJ_ASSERT(mal_list_len(list) == 3,
+                                   "Error: fn* expects 2 arguments. Got %d", mal_list_len(list)-1);
+                    mal_obj_t *params = *mal_list_get(list, 1);
+                    mal_obj_t *body = *mal_list_get(list, 2);
+                    EVAL_RETURN(mal_obj_function(params, body, env));
                 }
                 if (symblen == 2 && strncmp(symbol, "do", 2) == 0)
                 {
                     mal_obj_t *evaluated;
-                    int listlen = mal_list_len(list);
                     for (int i = 1; i < listlen-1; i++) {
                         evaluated = mal_eval(env, *mal_list_get(list, i));
                         mal_obj_release(evaluated);
@@ -238,14 +149,87 @@ mal_obj_t *mal_eval(mal_env_t *env, mal_obj_t *node) {
                     continue;
                 }
             }
-            /* standard apply case here */
-            EVAL_RETURN(mal_handle_apply(env, node));
+            // apply/invoke case
+            mal_obj_t *zeroth = *mal_list_get(list, 0);
+
+            mal_obj_t *func = mal_eval(env, zeroth);
+            if (func->type == MAL_ERROR) {
+                EVAL_RETURN(func);
+            }
+            if (!(func->type == MAL_BUILTIN || func->type == MAL_FUNCTION))
+            {
+                mal_obj_t *err = mal_obj_error_format(
+                    "Error: Parameter '%s' cannot be used as a function to evaluate",
+                    mal_obj_sprint(func));
+                mal_obj_release(func);
+
+                EVAL_RETURN(err);
+            }
+
+            mal_list_t *eval_args = mal_list_create(listlen-1);
+            for (int i = 1; i < listlen; i++) {
+                mal_obj_t **arg_ptr = mal_list_get(list, i);
+                mal_obj_t *arg = mal_eval(env, *arg_ptr);
+                if (arg->type == MAL_ERROR) {
+                    for (int j = 0; j < mal_list_len(eval_args); j++) {
+                        mal_obj_release(*(mal_list_get(eval_args, j)));
+                    }
+                    mal_list_free(eval_args);
+                    mal_obj_release(func);
+                    EVAL_RETURN(arg);
+                }
+                mal_list_push_back(eval_args, arg);
+            }
+
+            mal_obj_t *final_result = NULL;
+
+            if (func->type == MAL_BUILTIN) {
+                final_result = func->data.builtin_fn(eval_args);
+            }
+            else if (func->type == MAL_FUNCTION) {
+                mal_closure_t *f = func->data.function;
+                mal_obj_t *err_feedback = NULL;
+                mal_env_t *call_env = mal_env_create(f->env);
+
+                if (mal_env_bind(call_env, f->params->data.list, eval_args, &err_feedback)) {
+                    // save and retain body BEFORE relasing func
+                    mal_obj_t *body = f->body;
+                    mal_obj_retain(body);
+
+                    // cleanup eval_args and func
+                    for (int i = 0; i < mal_list_len(eval_args); i++) {
+                        mal_obj_release(*(mal_list_get(eval_args, i)));
+                    }
+                    mal_list_free(eval_args);
+                    mal_obj_release(func);
+
+                    // TCO
+                    mal_env_release(env);
+                    env = call_env;
+                    node = body; // body is still alive
+                    continue;
+                } else {
+                    final_result = err_feedback;
+                    mal_env_release(call_env);
+                }
+            }
+
+            // cleanup eval_args and func
+            for (int i = 0; i < mal_list_len(eval_args); i++) {
+                mal_obj_release(*(mal_list_get(eval_args, i)));
+            }
+            mal_list_free(eval_args);
+            mal_obj_release(func);
+
+            EVAL_RETURN(final_result);
         }
         break;
         default:
             break;
         }
-        EVAL_RETURN(mal_eval_noop(node));
+        // default noop case
+        mal_obj_retain(node);
+        EVAL_RETURN(node);
     }
 }
 
@@ -267,7 +251,7 @@ bool mal_rep(mal_reader_t *reader, mal_env_t *env) {
     mal_obj_t *result = mal_eval(env, root);
 
     free(input);
-    mal_print(result);
+    mal_obj_println(result);
     mal_obj_release(result);
     mal_obj_release(root);
 
